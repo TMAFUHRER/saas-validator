8f3f60d6cfe6aa0ded274d20a5b32b7c87bde814
Trafalgar Waterloo <26021972-TrafalgarWater1@users.noreply.replit.com>
2026-06-13 17:40:10 +0000

    fix vercel config

---
+++ artifacts/saas-validator/.next/server/chunks/803.js
@@ -0,0 +1,1 @@
+"use strict";exports.id=803,exports.ids=[803],exports.modules={84803:(a,b,c)=>{c.d(b,{clerkDevelopmentCache:()=>d.H2,createConfirmationMessage:()=>d.Br,createKeylessModeMessage:()=>d.Re});var d=c(82853)}};
